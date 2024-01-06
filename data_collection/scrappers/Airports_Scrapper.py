995965ec18a8c303c35103dddec01a55a6abaa07
AymaneSab <88624531+AymaneSab@users.noreply.github.com>
2024-01-06 14:03:34 +0100

    Add save_airports_data function

--- data_collection/scrappers/Airports_Scrapper.py
+++ data_collection/scrappers/Airports_Scrapper.py
@@ -2,6 +2,7 @@ import atexit
 import datetime
 import logging
 import os
+from hamcrest import none
 import requests
 import pandas as pd
 import sys 
@@ -15,7 +16,7 @@ class Airport_Scrapper:
             "X-RapidAPI-Key": self.api_key,
             "X-RapidAPI-Host": "airport-info.p.rapidapi.com"
         }
-        self.csv_path = "data_collection/data_sources/airports.csv"
+        self.csv_path = "data_collection/collected_data/airports_data.csv"
 
         self.logger = self.setup_logging()
 
@@ -42,56 +43,56 @@ class Airport_Scrapper:
             response = requests.get(url, headers=self.headers, timeout=10)
             response.raise_for_status()  # Vérifiez si la requête a réussi
             return response.json() if response.status_code == 200 else None
+        
         except requests.exceptions.RequestException as e:
             self.logger.error(f"Erreur de requête pour l'aéroport {iata_code}: {str(e)}")
             return None
 
     def scrape_airports_info(self):
-        df = None  # Initialisez df en dehors du bloc try
+        df = None  
+        airport_info_list = []
 
         try:
             airports_data = self.read_airports_iata()
 
-            if 'code' in airports_data.columns:
-                airport_info_list = []
+            for iata_code in airports_data['code']:
+                airport_info = self.fetch_airport_info(iata_code)
 
-                for iata_code in airports_data['code']:
-                    airport_info = self.fetch_airport_info(iata_code)
+                if airport_info:
+                    self.logger.info(f"Informations ajoutées pour l'aéroport {iata_code}")
+                    airport_info_list.append(airport_info)
+                else:
+                    self.logger.warning(f"Échec de la requête pour l'aéroport {iata_code}.")
 
-                    if airport_info:
-                        self.logger.info(f"Informations ajoutées pour l'aéroport {iata_code}")
-                        airport_info_list.append(airport_info)
-                    else:
-                        self.logger.warning(f"Échec de la requête pour l'aéroport {iata_code}.")
-
-                df = pd.DataFrame(airport_info_list)[['iata', 'icao', 'name', 'location', 'latitude', 'longitude', 'website'  , 'country', 'country_iso', 'phone']]
-
-                # Éliminez les lignes avec des valeurs manquantes dans l'un des champs
-                df = df.dropna(subset=['iata', 'icao', 'name', 'location', 'latitude', 'longitude', 'website' ,'city', 'country', 'country_iso', 'phone'])
-
-                df.to_csv("data_collection/data_sources/airport_info.csv", index=False)
-
-                self.logger.info("Les données ont été sauvegardées dans 'airport_info.csv'.")
-
-            else:
-                self.logger.error("La colonne 'code' n'existe pas dans le DataFrame.")
+            self.save_scrapped_airports(airport_info_list)
 
         except KeyboardInterrupt:
-            df = pd.DataFrame(airport_info_list)[['iata', 'icao', 'name', 'location', 'latitude', 'longitude', 'website' ,'city'  , 'country', 'country_iso', 'phone']]
-            # Éliminez les lignes avec des valeurs manquantes dans l'un des champs
-            df = df.dropna(subset=['iata', 'icao', 'name', 'location', 'latitude', 'longitude', 'website','city', 'country', 'country_iso', 'phone'])
-            df.to_csv("data_collection/data_sources/airport_info.csv", index=False)
             self.logger.info("Programme interrompu par l'utilisateur....Saving the current data ")
-            sys.exit(0)
+            self.save_scrapped_airports(airport_info_list)
 
         except Exception as e:
-            self.logger.error(f"Erreur lors de la sauvegarde des données collectées : {str(e)}")
+            self.logger.error(f"Exception thrown at  scrape_airports_info()  : {str(e)}")
+            self.save_scrapped_airports(airport_info_list)
 
         finally:
-            df = pd.DataFrame(airport_info_list)[['iata', 'icao', 'name', 'location', 'latitude', 'longitude', 'website','city', 'country', 'country_iso', 'phone']]
-            df = df.dropna(subset=['iata', 'icao', 'name', 'location', 'latitude', 'longitude', 'website','city', 'country', 'country_iso', 'phone'])
-            df.to_csv("data_collection/data_sources/airport_info.csv", index=False)
-            self.logger.info("Programme terminé.")
+            self.save_scrapped_airports(airport_info_list)
+
+    def save_scrapped_airports(self , airport_info_list):
+        try:
+            self.logger.info("Saving the scrapped airports data ........................................")
+
+            df = pd.DataFrame(airport_info_list)[['iata', 'icao', 'name', 'location', 'latitude', 'longitude', 'website'  , 'country', 'country_iso', 'phone']]
+
+            # Éliminez les lignes avec des valeurs manquantes dans l'un des champs
+            df = df.dropna(subset=['iata', 'icao', 'name', 'location', 'latitude', 'longitude', 'website' ,'city', 'country', 'country_iso', 'phone'])
+
+            df.to_csv("/Users/sabri/Desktop/Study /Youcode/Github/aviation-data-analytics/data_collection/collected_airports_data.csv", index=False)
+
+            self.logger.info("Les données ont été sauvegardées dans 'collected_airports_data.csv'.")
+
+        except Exception as e:
+            self.logger.error(f"Exception thrown at save_scrapped_airports : {e} ")
+            sys.exit(0)
 
 
 scrapper = Airport_Scrapper()
